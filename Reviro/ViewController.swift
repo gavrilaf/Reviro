9d4feafb5fd6893c50efec8d9199fc721762a8ee
Eugen Fedchenko <eugene.fedchenko@wirexapp.comeugene.fedchenko@wirexapp.comeugene.fedchenko@wirexapp.comeugene.fedchenko@wirexapp.comeugene.fedchenko@wirexapp.comeugene.fedchenko@wirexapp.comeugene.fedchenko@wirexapp.com>
2017-03-29 21:03:40 +0300

    Initial Commit

---
+++ Reviro/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Reviro
+//
+//  Created by Eugen Fedchenko on 3/29/17.
+//  Copyright © 2017 gavrilaf. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+

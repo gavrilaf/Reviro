70d89385dc09b6e47dbac170fb843d14e040b515
Eugen Fedchenko <eugene.fedchenko@wirexapp.comeugene.fedchenko@wirexapp.comeugene.fedchenko@wirexapp.comeugene.fedchenko@wirexapp.comeugene.fedchenko@wirexapp.comeugene.fedchenko@wirexapp.comeugene.fedchenko@wirexapp.com>
2017-04-17 22:01:05 +0300

    fixed project file

--- Sources/Reviro/List/LinkedList.swift
+++ Sources/Reviro/List/LinkedList.swift
@@ -10,17 +10,59 @@ import Foundation
 
 public final class LinkedList<E> {
     
+    public init() {
+        nilNode = Node<E>()
+    }
+    
+    
     class Node<E> {
-        let value: E
+        let value: E?
         var next: Node?
         var prev: Node?
+        
+        init() {
+            value = nil
+        }
         
         init(value: E) {
             self.value = value
         }
     }
     
+    let nilNode: Node<E>
+}
+
+extension LinkedList: DequeueProtocol {
     
+    public var isEmpty: Bool {
+        return first != nil
+    }
     
+    public var first: E? {
+        return nil
+    }
+    
+    public var last: E? {
+        return nil
+    }
+    
+    public func pushFirst(e: E) {
+    }
+    
+    public func pushLast(e: E) {
+    
+    }
+    
+    public func popFirst() -> E? {
+        return nil
+    }
+    
+    public func popLast() -> E? {
+        return nil
+    }
+    
+    public func clear() {
+    
+    }
 
 }
